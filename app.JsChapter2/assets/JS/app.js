fc624ffc6df3d2f47d3133a05fcca777d8dcb912
jaweria-dev <123859213+jaweria-dev@users.noreply.github.com>
2023-05-04 03:34:49 +0500

    Add files via upload

---
+++ app.JsChapter2/assets/JS/app.js
@@ -0,0 +1,73 @@
+//Question no 1
+
+// var username;
+
+//------------------------End of Question 1-----------------------------
+
+//End of Question 1
+
+//Question 2
+
+// var myName = "JAWERIA AHMED SIDDIQUI";
+
+//------------------------End of Question 2-----------------------------
+
+//Queation 3
+
+// var message = "Hello World";
+// alert(message);
+
+//------------------------End of Question 3-----------------------------
+
+//Question 4
+//Students Data
+
+// var firstName = "Johne";
+// var lastName = "Doe";
+// alert(firstName + " " + lastName)
+
+// var age = "15 years old";
+// alert(age);
+
+// var education = "Certified Mobile Application Development";
+// alert(education);
+
+//-------------------------End of Question 4-----------------------------
+
+//Question 5
+// var favFood="PIZZA \n PIZZ \n PIZ \n PI  \n P";
+// alert(favFood);
+
+//-----------------------------End of Question 5-------------------------
+
+//Question 6
+// var email="JIYA@gmail.com";
+// alert("My email address is " + email);
+
+//------------------------------End of Question 6-----------------------
+
+//End of Question 6
+
+//Question 7
+// var book = " A smarter way to learn JavaScript ";
+// alert("I am trying to learn from the Book" + book);
+
+//-----------------------------End of Question 7----------------------
+
+//Question 8
+// var content ="Yah! I can write HTML Content through JavaScript";
+// document.write(content);
+
+//-----------------------------End of Question 8------------------------------
+
+//Question 9
+// var end = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(end);
+
+//-----------------------------End of Question 9--------------------------------
+//End//
+
+
+
+
+
